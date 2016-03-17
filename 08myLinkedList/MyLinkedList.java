b03038e3914d7874ff0f684626baa9d9d48ec66b
Jackey Mei <Jackey Mei>
2016-03-16 20:16:47 -0400

    Made series of changes and added a lot of good methods

--- 08myLinkedList/MyLinkedList.java
+++ 08myLinkedList/MyLinkedList.java
@@ -13,19 +13,91 @@ public class MyLinkedList {
 	size = 1;
     }
 
+    public int size() {
+	return size;
+    }
+
+    public int indexOf(int value) {
+	LNode current = start;
+	int index = 0;
+	while (current.getNext() != null) {
+	    if (current.getValue() == value) {
+		return index;
+	    }
+	    index ++;
+	    current = current.getNext();
+	}
+	return -1;
+    }
+
+    public int remove(int index) {
+	if (index >= size || index < 0) {
+	    throw new IllegalArgumentException("index cannot be equal to or greater than size or less than 0");
+	}
+	LNode current = start;
+	LNode removed;
+	// sets the start to the second element
+	if (index == 0) {
+	    removed = start;
+	    start = current.getNext();
+	}
+	// removes element at index and redirects
+	else {
+	    for (int i = index; i > 1; i --) {
+		current = current.getNext();
+	    }
+	    removed = current.getNext();
+	    current.setNext(removed.getNext());
+	}
+	size --;
+	return removed.getValue();
+    }
+
     public boolean add(int value) {
 	LNode temp = new LNode(value);
 	LNode current = start;
-	// from the start LNode, go to the end of the list
+	// sets empty start to new node
 	if (current == null) {
 	    start = temp;
 	    size ++;
 	    return true;
 	}
+	// from the start LNode, go to the end of the list
 	while (current.getNext() != null) {
 	    current = current.getNext();
 	}
-	// last node becomes the new next
+	// last node becomes the new node
+	current.setNext(temp);
+	size ++;
+	return true;
+    }
+
+    public boolean add(int index, int value) {
+	LNode temp = new LNode(value);
+	LNode current = start;
+	if (index > size || index < 0) {
+	    throw new IllegalArgumentException("index cannot be equal to or greater than size or less than 0");
+	}
+	// sets empty start to new node
+	if (current == null) {
+	    start = temp;
+	    size ++;
+	    return true;
+	}
+	// adds new node at index 0
+	if (index == 0) {
+	    temp.setNext(current);
+	    start = temp;
+	    size ++;
+	    return true;
+	}
+	// accounts for cases where index is not 0
+	// new node sets next to node at index if that node exists
+	// node at index - 1 sets next to new node
+	for (int i = index; i > 1; i --) {
+	    current = current.getNext();
+	}
+	temp.setNext(current.getNext());
 	current.setNext(temp);
 	size ++;
 	return true;
@@ -51,7 +123,7 @@ public class MyLinkedList {
 
     public int get(int index) {
 	if (index >= size || index < 0) {
-	    throw new IllegalArgumentException("index cannot be bigger than size or less than 0");
+	    throw new IllegalArgumentException("index cannot be equal to or greater than size or less than 0");
 	}
 	LNode current = start;
 	for (int i = index; i > 0; i --) {
@@ -62,7 +134,7 @@ public class MyLinkedList {
 
     public void set(int index, int newValue) {
 	if (index >= size || index < 0) {
-	    throw new IllegalArgumentException("index cannot be bigger than sizeor less than 0");
+	    throw new IllegalArgumentException("index cannot be equal to or greater than size or less than 0");
 	}
 	LNode current = start;
 	for (int i = index; i > 0; i --) {
@@ -109,9 +181,12 @@ public class MyLinkedList {
 	myLL.add(7);
 	myLL.set(1, 50);
 	myLL.set(2, 25);
-	//myLL.set(3, 666);
 	System.out.println(myLL);
-	System.out.println(myLL.get(2));
+	myLL.add(3, 100);
+	//System.out.println(myLL.remove(0));
+	//myLL.remove(0);
+	//System.out.println(myLL);
+	
     }
 }
 
