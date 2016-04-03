fedc2fb56811fa0101382d4452b18cf3bec27d8c
Jackey Mei <Jackey Mei>
2016-04-03 17:29:11 -0400

    DOUBLE ENDED Q

---
+++ 10myDeque/MyDeque.java
@@ -0,0 +1,27 @@
+public class MyDeque<T> {
+    T[] data;
+
+    @SupressWarnings("unchecked")
+    public MyDeque() {
+	data = (T[]) new Object[10];
+    }
+
+    public void addFirst(T value) {
+    }
+
+    public void addLast(T value) {
+    }
+
+    public T removeFirst() {
+    }
+
+    public T removeLast() {
+    }
+
+    public T getFirst() {
+    }
+
+    public T getLast() {
+    }
+}
+    
