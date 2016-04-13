492e72f1633ea51ca75ba0e2f88400eab04572fd
Jackey Mei <Jackey Mei>
2016-04-12 20:18:05 -0400

    more STUFF

--- 11pathFinding/BetterMaze.java
+++ 11pathFinding/BetterMaze.java
@@ -3,31 +3,34 @@ import java.io.*;
 public class BetterMaze {
     private class Node {
 	Coordinate location;
-	Node last;
+	Node prev;
 
 	public Node() {
 	    location = new Coordinate();
-	    last = null;
+	    prev = null;
 	}
     
 	public Node(Coordinate location) {
 	    this.location = location;
-	    last = null;
+	    prev = null;
 	}
     
-	public Node(Node last) {
+	public Node(Node prev) {
 	    location = new Coordinate();
-	    this.last = last;
+	    this.prev = prev;
 	}
     
-	public Node(Coordinate location, Node last) {
+	public Node(Coordinate location, Node prev) {
 	    this.location = location;
-	    this.last = last;
+	    this.prev = prev;
 	}
 
-	public Node getLast() {
-	    return last;
+	public Node getPrev() {
+	    return prev;
 	}
+
+	public void setPrev(Node prev) {
+	    this.prev = prev;
     
 	public Coordinate getCoordinate() {
 	    return location;
@@ -38,45 +41,44 @@ public class BetterMaze {
     private int[] solution;
     private int startRow, startCol;
     private Frontier<Node> placesToGo;
-    private boolean animate; //default to false
+    private boolean animate;
 
-   /**return a COPY of solution.
-     *This should be : [x1,y1,x2,y2,x3,y3...]
-     *the coordinates of the solution from start to end.
-     *Precondition : one of the solveXXX methods has already been 
-     * called (solveBFS OR solveDFS OR solveAStar)
-     *(otherwise an empty array is returned)
-     *Postcondition:  the correct solution is in the returned array
-    **/
     public int[] solutionCoordinates() {
         return solution;
     }
 
-
-    /**initialize the frontier as a queue and call solve
-    **/
     public boolean solveBFS() {  
         placesToGo = new FrontierQueue<T>();
 	return solve();
-    }   
+    }
 
-
-   /**initialize the frontier as a stack and call solve
-    */ 
     public boolean solveDFS() {  
         placesToGo = new FrontierStack<T>();
 	return solve();
-    }    
+    }
 
-   /**Search for the end of the maze using the frontier. 
-      Keep going until you find a solution or run out of elements on the frontier.
-    **/
     private boolean solve() {  
-        placesToGo.add(new Node(new Coordinate(startRow, startCol)), null);
+        placesToGo.add(new Node(new Coordinate(startRow, startCol)));
 	while (placesToGo.hasNext()) {
+	    getNeightbors(placesToGo.next());
+	}
+    }
 
-    }    
-     
+    public void getNeighbors(Node current) {
+	if (maze[current.getCoordinate().getX() + 1][current.getCoordinate().getY()] == ' ') {
+	    placesToGo.add(new Node(new Coordinate(x + 1, y), current));
+	}
+	if (maze[current.getCoordinate().getX() - 1][current.getCoordinate().getY()] != ' ') {
+	    placesToGo.add(new Node(new Coordinate(x + 1, y), current));
+	}
+	if (maze[current.getCoordinate().getX()][current.getCoordinate().getY() + 1] != ' ') {
+	    placesToGo.add(new Node(new Coordinate(x + 1, y), current));
+	}
+	if (maze[current.getCoordinate().getX()][current.getCoordinate().getY() - 1] != ' ') {
+	    placesToGo.add(new Node(new Coordinate(x + 1, y), current));
+	}
+    }
+	
    /**mutator for the animate variable  **/
     public void setAnimate(boolean b) {
 	/** IMPLEMENT THIS **/
@@ -93,43 +95,42 @@ public class BetterMaze {
 	String ans = "";
 	try {
 	    Scanner in = new Scanner(new File(filename));
-
 	    //keep reading next line
-	    while(in.hasNext()) {
+	    while (in.hasNext()) {
 		String line = in.nextLine();
-		if(maxr == 0) {
+		if (maxr == 0) {
 		    //calculate width of the maze
 		    maxc = line.length();
 		}
 		//every new line add 1 to the height of the maze
-		maxr++;
+		maxr ++;
 		ans += line;
 	    }
 	}
-	catch(Exception e) {
+	catch (Exception e) {
 	    System.out.println("File: " + filename + " could not be opened.");
 	    e.printStackTrace();
 	    System.exit(0);
 	}
-	System.out.println(maxr+" "+maxc);
+	System.out.println(maxr + " " + maxc);
 	maze = new char[maxr][maxc];
-	for(int i = 0; i < ans.length(); i++) {
+	for (int i = 0; i < ans.length(); i ++) {
 	    char c = ans.charAt(i);
 	    maze[i / maxc][i % maxc] = c;
-	    if(c == 'S') {
+	    if (c == 'S') {
 		startCol = i % maxc;
 		startRow = i / maxc;
 	    }
 	}
     }
 
-    private static final String CLEAR_SCREEN =  "\033[2J";
-    private static final String HIDE_CURSOR =  "\033[?25l";
-    private static final String SHOW_CURSOR =  "\033[?25h";
-    private String go(int x,int y) {
+    private static final String CLEAR_SCREEN = "\033[2J";
+    private static final String HIDE_CURSOR = "\033[?25l";
+    private static final String SHOW_CURSOR = "\033[?25h";
+    private String go(int x, int y) {
 	return ("\033[" + x + ";" + y + "H");
     }
-    private String color(int foreground,int background) {
+    private String color(int foreground, int background) {
 	return ("\033[0;" + foreground + ";" + background + "m");
     }
 
@@ -145,27 +146,27 @@ public class BetterMaze {
 	}
     }
 
-
     public String toString() {
 	int maxr = maze.length;
 	int maxc = maze[0].length;
 	String ans = "";
-	if(animate) {
+	if (animate) {
 	    ans = "Solving a maze that is " + maxr + " by " + maxc + "\n";
 	}
-	for(int i = 0; i < maxc * maxr; i++) {
+	for (int i = 0; i < maxc * maxr; i ++) {
 	    if(i % maxc == 0 && i != 0) {
-		ans += color(37,40) + "\n";
+		ans += color(37, 40) + "\n";
 	    }
 	    char c =  maze[i / maxc][i % maxc];
-	    if(c == '#') {
-		ans += color(38,47)+c;
-	    }else {
-		ans += color(33,40)+c;
+	    if (c == '#') {
+		ans += color(38, 47) + c;
+	    }
+	    else {
+		ans += color(33, 40) + c;
 	    }
 	}
 	//nice animation string
-	if(animate) {
+	if (animate) {
 	    return HIDE_CURSOR + go(0, 0) + ans + color(37, 40) + "\n" + SHOW_CURSOR + color(37, 40);
 	}
 	else {
